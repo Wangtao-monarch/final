cd82495f6b0f551a598ea178ea2a8411402a61db
Wangtao-monarch <56293457+Wangtao-monarch@users.noreply.github.com>
2020-01-07 00:26:32 -0800

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+#include <ctype.h>
+#include<stdlib.h>
 
+int isinteger(char *in)
+{
+    for(int i=0;in[i];++i) {
+        if(!isdigit(in[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(int argc,char** argv)
+{
+    if(argc <= 1) {
+        puts("The program needs at least one integer parameter to run !");
+        return 1;
+    }
+
+    int min = 99999999;
+
+    for(int i=1;i<argc;i++) {
+        if(!isinteger(argv[i])) {
+            puts("The parameter has be an integer format!");
+            return ;
+        }
+
+       
+    }
+
+    
+}
