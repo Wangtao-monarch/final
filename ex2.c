e43db3ac3b87c45b4c88bd918ccbe3db864dbc8a
Wangtao-monarch <56293457+Wangtao-monarch@users.noreply.github.com>
2020-01-06 23:44:29 -0800

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<string.h>
 
+
+int main()
+{
+    char input[100];
+
+    gets(input);
+
+    for(int i=strlen(input) - 1; i >= 0; i--) putchar(input[i]);
+
+    return 0;
+}
